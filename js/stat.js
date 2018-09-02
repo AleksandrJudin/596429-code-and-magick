07218a482001e0947b25529f60d9a39531dcd090
justedoit <justedoit92@gmail.com>
2018-09-02 19:52:24 +0300

    функция окна

--- js/stat.js
+++ js/stat.js
@@ -8,4 +8,18 @@ window.renderStatistics = function (ctx, names, times) {
   var INITIAL_Y = 250;
   var maxElement;
   var step;
+
+  var drawCloud = function () {
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+    ctx.fillRect(110, 20, 420, 270);
+    ctx.fillStyle = 'rgba(255, 255, 255, 1)';
+    ctx.fillRect(100, 10, 420, 270);
+
+    ctx.fillStyle = 'rgba(0, 0, 0, 1)';
+    ctx.font = '16px PT Mono';
+    ctx.fillText('Ура вы победили!', 120, 40);
+    ctx.fillText('Список результатов:', 120, 60);
+  };
+
+  
 };
