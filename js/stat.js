37818e809eeb8c1023061c77ab2162213eddbe5e
justedoit <justedoit92@gmail.com>
2018-09-02 19:51:55 +0300

    определение переменных

---
+++ js/stat.js
@@ -0,0 +1,11 @@
+'use strict';
+
+window.renderStatistics = function (ctx, names, times) {
+  var HISTOGRAM_WIDTH = 150;
+  var BAR_WIDTH = 40;
+  var INDENT = BAR_WIDTH + 50;
+  var INITIAL_X = 140;
+  var INITIAL_Y = 250;
+  var maxElement;
+  var step;
+};
